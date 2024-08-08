48542d484f4657d40e3ed822a7a13f854076f021
Rahmn-Dev <sidqi.rahman@gmail.com>
2024-08-08 15:05:40 +0000

    updating apps

--- graphlinex/settings.py
+++ graphlinex/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'graphlinex.urls'
+ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
     {
