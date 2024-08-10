22550531838209b53f4bb19d53875d000cb7a82d
Rahmn-Dev <sidqi.rahman@gmail.com>
2024-08-10 17:16:31 +0000

    improvement history

--- app/views.py
+++ app/views.py
@@ -3,22 +3,55 @@ from .algorithm_dda import generate_plot as dda_generate_plot
 from .algorithm_bresenham import generate_plot as bresenham_generate_plot
 from .algorithm_midpoint import generate_plot as midpoint_generate_plot
 from .models import DataSubmission
+import re
+
+def parse_query(query):
+    midpoint_match = re.match(r'midpoint\((\d+),(\d+)\)\((\d+)\)', query)
+    if midpoint_match:
+        algorithm = 'midpoint'
+        x_center = int(midpoint_match.group(1))
+        y_center = int(midpoint_match.group(2))
+        radius = int(midpoint_match.group(3))
+        return algorithm, x_center, y_center, radius
+        
+    match = re.match(r'(\w+)\((\d+),(\d+)\)\((\d+),(\d+)\)', query)
+    if match:
+        algorithm = match.group(1)
+        x1 = int(match.group(2))
+        y1 = int(match.group(3))
+        x2 = int(match.group(4))
+        y2 = int(match.group(5))
+        return algorithm, x1, y1, x2, y2
+    
+    return None
+
 
 def draw_line(request):
     plot_json = None
     submissions = DataSubmission.objects.all().order_by('-created_at')
+
+    show_graph_query = request.GET.get('show_graph')
+    if show_graph_query:
+        parsed_data = parse_query(show_graph_query)
+        if parsed_data:
+            if parsed_data[0] == 'midpoint':
+                algorithm, x_center, y_center, radius = parsed_data
+                plot_json = midpoint_generate_plot(x_center, y_center, radius)
+                print(f"Graph generated for {algorithm} algorithm with center ({x_center}, {y_center}) and radius {radius}")
+            else:
+                algorithm, x1, y1, x2, y2 = parsed_data
+                if algorithm == 'dda':
+                    plot_json = dda_generate_plot(x1, y1, x2, y2)
+                elif algorithm == 'bresenham':
+                    plot_json = bresenham_generate_plot(x1, y1, x2, y2)
+                print(f"Graph generated for {algorithm} algorithm with coordinates ({x1}, {y1}) and ({x2}, {y2})")
+
+
     if request.method == 'POST':
         
 
         algorithm = request.POST.get('algorithm')
 
-        if algorithm == 'dda':
-            x1 = int(request.POST.get('x1'))
-            y1 = int(request.POST.get('y1'))
-            x2 = int(request.POST.get('x2'))
-            y2 = int(request.POST.get('y2'))
-            plot_json = dda_generate_plot(x1, y1, x2, y2)
-            print("ini dda")
         if algorithm == 'midpoint':
             x_center = int(request.POST.get("x_center"))
             y_center = int(request.POST.get("y_center"))
